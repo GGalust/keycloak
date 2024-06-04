0d98778b9a56a5b6452061f2212a6b0c21cc6090
Galust Grigoryan <127393964+GGalust@users.noreply.github.com>
2024-06-04 13:57:25 +0400

    first commit

---
+++ src/Login.tsx
@@ -0,0 +1,29 @@
+const Login = () => {
+    const handleClick = () => {
+        window.location.href = "http://localhost:9090/realms/cognaize-realm/protocol/openid-connect/auth?client_id=worktimal-workflow-client&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2F&response_mode=fragment&response_type=code&scope=openid";
+    }
+
+    const handleAPICall = async () => {
+        const response = await fetch('http://localhost:8087/workflow-api/api/v1/oa/test');
+        const json = await response.json();
+        console.log({json})
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick} style={{
+                height: '200px',
+                width: '1000px',
+                background: 'red'
+            }}>Login
+            </button>
+            <button onClick={handleAPICall}  style={{
+                height: '200px',
+                width: '1000px',
+                background: 'blue'
+            }}>API</button>
+        </div>
+    )
+}
+
+export default Login;
