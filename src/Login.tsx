6de0a2e7a0a150671650fe5d4fc6580b966e6eea
Galust Grigoryan <127393964+GGalust@users.noreply.github.com>
2024-06-06 16:23:59 +0400

    1

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,24 +1,42 @@
 import useAuth from "./hooks/useAuth.ts";
+import axios from "axios";
 
 const Login = () => {
     const client  = useAuth();
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        await client.init({onLoad:'login-required',checkLoginIframe: false});
-        //window.location.href = "http://localhost:9090/realms/cognaize-realm/protocol/openid-connect/auth?client_id=worktimal-workflow-client&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2F&response_mode=fragment&response_type=code&scope=openid";
-    }
+    // const handleClick = async (e) => {
+    //     e.preventDefault();
+    //     await client.init({onLoad:'login-required',checkLoginIframe: false});
+    // }
 
     const handleAPICall = async () => {
-        const response = await fetch('http://localhost:8087/workflow-api/api/v1/oa/test',{
-            credentials: "include",
-        });
-        const json = await response.json();
+        try {
+            console.log('token',client.token)
+            axios.get('http://localhost:8087/workflow-api/api/v1/oa/test', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${client.token}`
+                },
+                withCredentials: true  // If you're making requests that require credentials
+            })
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+
+
+        }
+        catch(e){
+            console.log(e)
+        }
+
     }
 
     return (
         <div>
-            <button onClick={handleClick} style={{
+            <button  style={{
                 height: '200px',
                 width: '1000px',
                 background: 'red'
